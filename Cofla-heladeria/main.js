cff1cfd22a70721b9a6db64ab4eb96c840f08e7f
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-01-12 23:15:30 -0300

    el vuelto del helado de cofla

--- Cofla-heladeria/main.js
+++ Cofla-heladeria/main.js
@@ -2,18 +2,26 @@ dineroCofla = prompt("¿Cuanto dinero tiene Cofla?");
 dineroRoberto = prompt("¿Cuanto dinero tiene Roberto?");
 dineroPedro = prompt("¿Cuanto dinero tiene Pedro?");
 
+let dinero = parseInt(dineroCofla);
+
 if (dineroCofla >= 0.6 && dineroCofla <= 1) {
   alert("Comprate un helado de agua");
-} else if (dineroCofla >= 1 && dineroCofla <= 1.5) {
+  alert("y tu vuelto es: " + (dinero - 0.6));
+} else if (dineroCofla >= 1 && dineroCofla <= 1.6) {
   alert("Comprate un helado de crema");
+  alert("y tu vuelto es: " + (dinero - 1));
 } else if (dineroCofla >= 1.6 && dineroCofla <= 1.7) {
   alert("Comprate un helado de heladix");
+  alert("y tu vuelto es: " + (dinero - 1.6));
 } else if (dineroCofla >= 1.7 && dineroCofla <= 1.8) {
   alert("Comprate un helado de heladovich");
-} else if (dineroCofla >= 1.8 && dineroCofla <= 2) {
+  alert("y tu vuelto es: " + (dinero - 1.7));
+} else if (dineroCofla >= 1.8 && dineroCofla <= 2.9) {
   alert("Comprate un helado de helardo");
-} else if (dineroCofla >= 2.9 && dineroCofla <= 4) {
+  alert("y tu vuelto es: " + (dinero - 1.8));
+} else if (dineroCofla >= 2.9 && dineroCofla <= 3) {
   alert("Comprate un helado de confites o un pote de 1/4");
+  alert("y tu vuelto es: " + (dinero - 2.9));
 } else {
   alert("ERES POBREEEEEEEEEE");
 }
@@ -34,22 +42,18 @@ if (dineroPedro >= 0.6 && dineroPedro <= 1.5) {
   alert("ERES POBREEEEEEEEEE");
 }
 
-
 if (dineroRoberto >= 0.6 && dineroRoberto < 1.5) {
-    alert("Comprate un helado de agua");
-  } else if (dineroRoberto >= 1 && dineroRoberto <= 1.5) {
-    alert("Comprate un helado de crema");
-  } else if (dineroRoberto >= 1.6 && dineroRoberto <= 1.7) {
-    alert("Comprate un helado de heladix");
-  } else if (dineroRoberto >= 1.7 && dineroRoberto <= 1.8) {
-    alert("Comprate un helado de heladovich");
-  } else if (dineroRoberto >= 1.8 && dineroRoberto <= 2) {
-    alert("Comprate un helado de helardo");
-  } else if (dineroRoberto >= 2.9 && dineroRoberto <= 4) {
-    alert("Comprate un helado de confites o un pote de 1/4");
-  } else {
-    alert("ERES POBREEEEEEEEEE");
-  }
-
-
-  
+  alert("Comprate un helado de agua");
+} else if (dineroRoberto >= 1 && dineroRoberto <= 1.5) {
+  alert("Comprate un helado de crema");
+} else if (dineroRoberto >= 1.6 && dineroRoberto <= 1.7) {
+  alert("Comprate un helado de heladix");
+} else if (dineroRoberto >= 1.7 && dineroRoberto <= 1.8) {
+  alert("Comprate un helado de heladovich");
+} else if (dineroRoberto >= 1.8 && dineroRoberto <= 2) {
+  alert("Comprate un helado de helardo");
+} else if (dineroRoberto >= 2.9 && dineroRoberto <= 4) {
+  alert("Comprate un helado de confites o un pote de 1/4");
+} else {
+  alert("ERES POBREEEEEEEEEE");
+}
